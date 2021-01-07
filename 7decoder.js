ab4e9e86e7d5d242ff79955bb7b0a82f37b135c6
nulla2011 <nulla@outlook.com>
2021-01-07 23:15:44 +0800

    crypto

--- 7decoder.js
+++ 7decoder.js
@@ -1,51 +1,61 @@
-const fs=require('fs');
+const fs = require('fs');
 let crypto;
 try {
-    crypto=require('crypto');
+    crypto = require('crypto');
 } catch (error) {
     console.error("No crypto!");
 }
+let Duplex = require('stream').Duplex;
+function bufferToStream(buffer) {
+    let stream = new Duplex();
+    stream.push(buffer);
+    stream.push(null);
+    return stream;
+}
 
-const args=process.argv.slice(2);
-let path=args[0];
+const args = process.argv.slice(2);
+let path = args[0];
 
-function getKey(){
-    let seed=37;
-    const array="J9h4j5eNds+aq1==".split("");
-    let array2=new ArrayBuffer(16);
+let getKey = () => {
+    let seed = 37;
+    const array = "J9h4j5eNds+aq1==".split("");
+    let array2 = Buffer.alloc(16);
     for (let i = 0; i < array.length; i++) {
-        array2[i]=array[i].charCodeAt()^seed;
-        seed+=13;
+        array2[i] = array[i].charCodeAt() ^ seed;
+        seed += 13;
     }
     return array2;
 }
-let key=getKey();
-let IV;
-fs.stat(path,function(err,stats){
-    const fe=fs.openSync(path,'r');
+let IV = Buffer.alloc(16);
+fs.stat(path, function (err, stats) {
+    const fe = fs.openSync(path, 'r');
     if (err) {
         console.log("file open error!");
     }
-    if (!err&&stats.isFile()){    //无错且为文件
-        let bh=Buffer.alloc(7);
-        fs.read(fe,bh,0,7,0,function(err,bytesRead,buffer){
-            if(err){
+    if (!err && stats.isFile()) {    //无错且为文件
+        let bh = Buffer.alloc(7);
+        fs.read(fe, bh, 0, 7, 0, function (err, bytesRead, buffer) {
+            if (err) {
                 console.error(err);
             }
-            let header=String(buffer);
-            if (header!="t7s-enc") {
+            let header = String(buffer);
+            if (header != "t7s-enc") {
                 console.error("not a enc file!");
             }
         });
-        let biv=Buffer.alloc(16);
-        fs.read(fe,biv,0,16,7,function(err,bytesRead,buffer){
-            if(err){
+        let biv = Buffer.alloc(16);
+        fs.read(fe, biv, 0, 16, 7, function (err, bytesRead, buffer) {
+            if (err) {
                 console.error(err);
             }
-            IV=buffer;
+            IV = buffer;
         })
-        let encData=fs.readFileSync(path).slice(7+16);
-        const algorithm="aes-128-cbc";
+        let encData = fs.readFileSync(path).slice(7 + 16);
+        const algorithm = "aes-128-cbc";
+        const key = getKey();
+        const decipher = crypto.createDecipheriv(algorithm, key, IV);
+        const output = fs.createWriteStream('test.jpg');
+        bufferToStream(encData).pipe(decipher).pipe(output);
     }
 });
 
