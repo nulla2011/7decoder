e7d473fc172ca7dcba299de7716fbb3f9f9000b9
nulla2011 <nulla@outlook.com>
2021-01-07 21:55:39 +0800

    crypto or crypto-js

--- 7decoder.js
+++ 7decoder.js
@@ -1,20 +1,34 @@
 const fs=require('fs');
+let crypto;
+try {
+    crypto=require('crypto');
+} catch (error) {
+    console.error("No crypto!");
+}
 
 const args=process.argv.slice(2);
-console.log(args[0]);
-function getkey(){
-    const seed=37;
-    
+let path=args[0];
+
+function getKey(){
+    let seed=37;
+    const array="J9h4j5eNds+aq1==".split("");
+    let array2=new ArrayBuffer(16);
+    for (let i = 0; i < array.length; i++) {
+        array2[i]=array[i].charCodeAt()^seed;
+        seed+=13;
+    }
+    return array2;
 }
-const IV;
-fs.stat(args[0],function(err,stats){
-    const fe=fs.openSync(args[0],'r');
+let key=getKey();
+let IV;
+fs.stat(path,function(err,stats){
+    const fe=fs.openSync(path,'r');
     if (err) {
         console.log("file open error!");
     }
     if (!err&&stats.isFile()){    //无错且为文件
-        let b=Buffer.alloc(7);
-        fs.read(fe,b,0,7,0,function(err,bytesRead,buffer){
+        let bh=Buffer.alloc(7);
+        fs.read(fe,bh,0,7,0,function(err,bytesRead,buffer){
             if(err){
                 console.error(err);
             }
@@ -23,7 +37,15 @@ fs.stat(args[0],function(err,stats){
                 console.error("not a enc file!");
             }
         });
-        
+        let biv=Buffer.alloc(16);
+        fs.read(fe,biv,0,16,7,function(err,bytesRead,buffer){
+            if(err){
+                console.error(err);
+            }
+            IV=buffer;
+        })
+        let encData=fs.readFileSync(path).slice(7+16);
+        const algorithm="aes-128-cbc";
     }
 });
 
