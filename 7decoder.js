0a8ae0d10a1325832551cc6b85aa7d3cf4d11858
nulla2011 <nulla@outlook.com>
2021-01-08 13:07:22 +0800

    use crypto

--- 7decoder.js
+++ 7decoder.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 let crypto;
 try {
     crypto = require('crypto');
@@ -14,7 +15,14 @@ function bufferToStream(buffer) {
 }
 
 const args = process.argv.slice(2);
-let path = args[0];
+let encPath = args[0];
+let outputPath = '';         //确定输出目录
+if (path.extname(encPath) == '.enc') {
+    outputPath = path.join(path.dirname(encPath), path.basename(encPath, '.enc'));
+}
+else {
+    outputPath = path.join(path.dirname(encPath), (path.parse(encPath).name + '_decoded' + path.extname(encPath)));
+}
 
 let getKey = () => {
     let seed = 37;
@@ -26,13 +34,12 @@ let getKey = () => {
     }
     return array2;
 }
-let IV = Buffer.alloc(16);
-fs.stat(path, function (err, stats) {
-    const fe = fs.openSync(path, 'r');
+fs.stat(encPath, function (err, stats) {
     if (err) {
         console.log("file open error!");
     }
     if (!err && stats.isFile()) {    //无错且为文件
+        const fe = fs.openSync(encPath, 'r');
         let bh = Buffer.alloc(7);
         fs.read(fe, bh, 0, 7, 0, function (err, bytesRead, buffer) {
             if (err) {
@@ -43,19 +50,19 @@ fs.stat(path, function (err, stats) {
                 console.error("not a enc file!");
             }
         });
-        let biv = Buffer.alloc(16);
-        fs.read(fe, biv, 0, 16, 7, function (err, bytesRead, buffer) {
+        let IV = Buffer.alloc(16);
+        fs.read(fe, IV, 0, 16, 7, function (err, bytesRead, buffer) {
             if (err) {
                 console.error(err);
             }
-            IV = buffer;
-        })
-        let encData = fs.readFileSync(path).slice(7 + 16);
+            //console.log(IV.toString());
+        });
+        let encData = fs.readFileSync(encPath).slice(7 + 16);   //剩下的是需要解密的数据
         const algorithm = "aes-128-cbc";
         const key = getKey();
         const decipher = crypto.createDecipheriv(algorithm, key, IV);
-        const output = fs.createWriteStream('test.jpg');
+        const output = fs.createWriteStream(outputPath);
         bufferToStream(encData).pipe(decipher).pipe(output);
+        console.log('success!');
     }
 });
-
